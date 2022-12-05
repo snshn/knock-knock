5b705a8a13c2acc8117203b75afdcde037365e11
Sunshine <sunshine@uberspace.net>
2022-12-04 19:34:30 -0500

    switch to using RDAP instead of WHOIS

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+mod utils;
+
 use chrono::{DateTime, Duration, Utc};
 use clap::{Arg, Command};
+use rdap_client::bootstrap::Bootstrap;
+use rdap_client::parser::{Domain, EventAction};
+use rdap_client::Client;
 use std::env;
-use whois_rust::{WhoIs, WhoIsLookupOptions};
-use whoisthere::{parse_info, DomainProps};
+
+use crate::utils::pluralize;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum KnockKnockError {
+    DomainNotFound,
+    NoServersFound,
+    UnableToObtainResponseFromServer,
+    UnableToRetrieveListOfServers,
+}
 
 const ANSI_COLOR_RED: &'static str = "\x1b[31m";
 const ANSI_COLOR_GREEN: &'static str = "\x1b[32m";
@@ -11,14 +24,6 @@ const ANSI_COLOR_RESET: &'static str = "\x1b[0m";
 const EXPIRATION_WARNING: i64 = 2419200; // Amount of seconds in 4 weeks
 const EXPIRATION_CRITICAL: i64 = 604800; // Amount of seconds in 1 week
 const INDENTATION: &'static str = "    ";
-
-fn pluralize(item_name: &str, quantity: i64) -> String {
-    let mut result = String::from(item_name);
-    if quantity != 1 {
-        result += "s";
-    }
-    result
-}
 
 fn compose_readable_duration(mut duration: Duration, show_full_time: bool) -> String {
     let is_neg: bool = duration.num_milliseconds() < 0;
@@ -80,17 +85,35 @@ fn compose_readable_duration(mut duration: Duration, show_full_time: bool) -> St
     vec.join(", ")
 }
 
-pub fn get_domain_info(domain_name: &str) -> Result<DomainProps, std::fmt::Error> {
-    static JSON: &str = include_str!("servers.json");
-    let whois: WhoIs = WhoIs::from_string(JSON).unwrap();
-
-    match whois.lookup(WhoIsLookupOptions::from_string(domain_name).unwrap()) {
-        Ok(result) => Ok(parse_info(&domain_name, &result)),
-        Err(_e) => Err(std::fmt::Error),
+async fn check_domain(
+    client: &Client,
+    bootstrap: &Bootstrap,
+    domain_name: &str,
+) -> Result<Domain, KnockKnockError> {
+    if let Some(servers) = bootstrap.dns.find(&domain_name) {
+        if servers.len() > 0 {
+            match client.query_domain(&servers[0], domain_name).await {
+                Ok(response) => {
+                    return Ok::<Domain, KnockKnockError>(response);
+                }
+                Err(_error) => {
+                    // if error.Rdap() == 404 {
+                    return Err::<Domain, KnockKnockError>(KnockKnockError::DomainNotFound);
+                    // } else {
+                    // return Err::<Domain, KnockKnockError>(KnockKnockError::UnableToObtainResponseFromServer);
+                    // }
+                }
+            }
+        } else {
+            return Err::<Domain, KnockKnockError>(KnockKnockError::NoServersFound);
+        }
+    } else {
+        return Err::<Domain, KnockKnockError>(KnockKnockError::UnableToRetrieveListOfServers);
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let matches = Command::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -110,79 +133,88 @@ fn main() {
         )
         .get_matches();
 
+    let client: Client = Client::new();
+    let bootstrap: Bootstrap = client.fetch_bootstrap().await.unwrap();
+
     let iterator = matches.values_of("DOMAINS");
     for domain_name in iterator.unwrap() {
         println!("{}:", domain_name);
-        match get_domain_info(domain_name) {
-            Ok(domain_whois_info) => {
-                if domain_whois_info.is_registered {
-                    match domain_whois_info.expiration_date.parse::<DateTime<Utc>>() {
-                        Ok(expiration_date) => {
-                            let now: DateTime<Utc> =
-                                DateTime::<Utc>::from_utc(Utc::now().naive_utc(), Utc);
-                            let time_diff: Duration = expiration_date - now;
 
-                            let is_neg: bool = time_diff.num_milliseconds() < 0;
-                            let seconds: i64 = time_diff.num_seconds();
-                            let is_warning: bool = seconds <= EXPIRATION_WARNING;
-                            let is_critical: bool = seconds <= EXPIRATION_CRITICAL;
-                            let color;
+        let result = check_domain(&client, &bootstrap, &domain_name).await;
+        match result {
+            Ok(response) => {
+                let mut found_expiration_date_info = false;
+                let mut expiration_date: DateTime<Utc> =
+                    DateTime::<Utc>::from_utc(Utc::now().naive_utc(), Utc);
+                for event in response.events.into_iter() {
+                    // println!("{:?}", event);
+                    if event.event_action == EventAction::Expiration {
+                        expiration_date = event.event_date.with_timezone(&Utc);
+                        found_expiration_date_info = true;
+                        break;
+                    }
+                }
 
-                            if is_neg {
-                                color = ANSI_COLOR_RED;
-                            } else {
-                                if is_warning {
-                                    color = ANSI_COLOR_YELLOW;
-                                } else if is_critical {
-                                    color = ANSI_COLOR_RED;
-                                } else {
-                                    color = ANSI_COLOR_GREEN;
-                                }
-                            }
-                            if expiration_date >= now {
-                                println!(
-                                    "{}{}Domain name will expire in {}{}",
-                                    INDENTATION,
-                                    color,
-                                    compose_readable_duration(
-                                        time_diff,
-                                        matches.is_present("fulltime")
-                                    ),
-                                    ANSI_COLOR_RESET,
-                                );
-                            } else {
-                                println!(
-                                    "{}{}Domain name has expired {} ago{}",
-                                    INDENTATION,
-                                    color,
-                                    compose_readable_duration(
-                                        time_diff,
-                                        matches.is_present("fulltime")
-                                    ),
-                                    ANSI_COLOR_RESET,
-                                );
-                            }
-                        }
-                        Err(_e) => {
-                            println!(
-                                "{}{}Unable to obtain domain name expiration date{}",
-                                INDENTATION, ANSI_COLOR_RED, ANSI_COLOR_RESET,
-                            );
+                if found_expiration_date_info {
+                    let now: DateTime<Utc> = DateTime::<Utc>::from_utc(Utc::now().naive_utc(), Utc);
+                    let time_diff: Duration = expiration_date - now;
+
+                    let is_neg: bool = time_diff.num_milliseconds() < 0;
+                    let seconds: i64 = time_diff.num_seconds();
+                    let is_warning: bool = seconds <= EXPIRATION_WARNING;
+                    let is_critical: bool = seconds <= EXPIRATION_CRITICAL;
+                    let color;
+
+                    if is_neg {
+                        color = ANSI_COLOR_RED;
+                    } else {
+                        if is_warning {
+                            color = ANSI_COLOR_YELLOW;
+                        } else if is_critical {
+                            color = ANSI_COLOR_RED;
+                        } else {
+                            color = ANSI_COLOR_GREEN;
                         }
                     }
+                    if expiration_date >= now {
+                        println!(
+                            "{}{}Domain name will expire in {}{}",
+                            INDENTATION,
+                            color,
+                            compose_readable_duration(time_diff, matches.is_present("fulltime")),
+                            ANSI_COLOR_RESET,
+                        );
+                    } else {
+                        println!(
+                            "{}{}Domain name has expired {} ago{}",
+                            INDENTATION,
+                            color,
+                            compose_readable_duration(time_diff, matches.is_present("fulltime")),
+                            ANSI_COLOR_RESET,
+                        );
+                    }
                 } else {
+                    println!(
+                        "{}{}Unable to obtain domain name expiration date{}",
+                        INDENTATION, ANSI_COLOR_RED, ANSI_COLOR_RESET,
+                    );
+                }
+            }
+            Err(error) => match error {
+                KnockKnockError::DomainNotFound => {
                     println!(
                         "{}{}Domain name not registered{}",
                         INDENTATION, ANSI_COLOR_GREEN, ANSI_COLOR_RESET,
                     );
                 }
-            }
-            Err(_e) => {
-                println!(
-                    "{}{}Unable to retrieve domain whois information{}",
-                    INDENTATION, ANSI_COLOR_RED, ANSI_COLOR_RESET,
-                );
-            }
+                _ => {
+                    println!(
+                        "{}{}Unable to retrieve domain name info{}",
+                        INDENTATION, ANSI_COLOR_RED, ANSI_COLOR_RESET,
+                    );
+                    continue;
+                }
+            },
         }
     }
 }
